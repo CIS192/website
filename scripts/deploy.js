44cd2d78c49a7ab87cf59829fe8adf83e647e8d9
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2019-12-21 22:54:19 -0500

    Fix deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,7 +4,7 @@ ghpages.publish(
   'public',
   {
     branch: 'master',
-    repo: 'https://github.com/kirubarajan/kirubarajan.github.io.git',
+    repo: 'https://github.com/cis192/cis192.github.io.git',
   },
   () => {
     console.log('Deploy Complete!')

0eb10bc689ee729c37c8ac5206d0a335bf65e017
Daniel Tao <dtao@seas.upenn.edu>
2020-01-27 16:14:00 -0500

    Add Daniel to staff.js

--- src/pages/staff.js
+++ src/pages/staff.js
@@ -13,7 +13,7 @@ const staff_list = [
     image: "https://i.imgur.com/mP7wT9v.jpg",
     name: "Amit Lohe",
     role: "Teaching Assistant",
-    bio: "Amit a junior studying CIS and *Wharton* from Lexington, KY. He enjoy cooking, reading, and playing squash. He likes to eat Indian food.",
+    bio: "Amit a junior studying CIS and *Wharton* from Lexington, KY. He enjoys cooking, reading, and playing squash. He likes to eat Indian food.",
     hours: "Wednesdays, 3:00 to 5:00 @ Levine 5th Floor"
   },
   {
@@ -22,7 +22,14 @@ const staff_list = [
     role: "Teaching Assistant",
     bio: "Kevin is a sophomore from Cambridge, MA studying CIS and Statistics. He enjoys skiing and watching NBA. His favourite programming language is Python.",
     hours: "Monday, 4:30 to 6:30 PM @ Rodin Mezzanine"
-  }
+  },
+  {
+    image: "https://danxtao.com/assets/headshot4-3.png",
+    name: "Daniel Tao",
+    role: "Teaching Assistant",
+    bio: "Daniel is a sophomore from Chicago, IL studying Networked and Social Systems Engineering. He likes rhythm games, reading, pranks, and meaningful comedy.",
+    hours: "Thursdays, 1:30 to 3:00 PM @ Weiss Tech House"
+  },
 ]
 
 const Staff = () => {
